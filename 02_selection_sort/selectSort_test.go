cdd37034f0ba015167363e434f24c36865379fbc
Ben Stone <shiqstone@gmail.com>
2021-02-24 10:02:39 +0800

    Create selectSort_test.go

---
+++ 02_selection_sort/selectSort_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_selectionSort(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "test1",
+			args: args{
+				arr: []int{5, 3, 6, 2, 10},
+			},
+			want: []int{2, 3, 5, 6, 10},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := selectionSort(tt.args.arr); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("selectionSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
